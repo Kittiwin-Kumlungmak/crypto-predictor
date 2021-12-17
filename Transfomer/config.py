c16bd9520cc14e61124690edff4839e6488681dc
Kittiwin-Kumlungmak <kittiwin.kumlungmak@gmail.com>
2021-12-17 08:09:51 +0700

    rm

--- Transfomer/config.py
+++
@@ -1,3 +0,0 @@
-bnb_api_key = 'gpK0jBSVjE7foKq6f4XWhu62noj3GWv90Swv8CKsFDsBxTodtvs5Sd2pGy1NZblB'
-bnb_api_secret = 'Zd28WyeRww6maVfQFYFcZO2W7XFGBZ4FNdYBhLt9Jtsl89SKgCCS9CNxBSdQNDGP'
-app_secret_key = b'gnkjJKJIEigkibjgjOPJTlpIorkgopfklkJfkgopdklgkLKldgo'
